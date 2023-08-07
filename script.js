65fb33e95dadd6e1a74983e073e77187a2ac5bd9
nm1008 <mallari08nikko@yahoo.com>
2023-08-08 02:41:11 +0800

    added object constructor function

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const newBook = document.querySelector(".new-book");
 
 const formContainer = document.querySelector(".form-container");
 
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+ 
+}
+
+
+
+
 
 newBook.addEventListener("click", () => {
     formContainer.style.display = "flex"
