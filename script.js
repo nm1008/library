5b41d1903eddedbfc1f83ed79513884de09a34a4
nm1008 <mallari08nikko@yahoo.com>
2023-08-08 02:39:30 +0800

    added event listener for new book

---
+++ script.js
@@ -0,0 +1,8 @@
+const newBook = document.querySelector(".new-book");
+
+const formContainer = document.querySelector(".form-container");
+
+
+newBook.addEventListener("click", () => {
+    formContainer.style.display = "flex"
+    })
