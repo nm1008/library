5bb932c336e5f530f69840f69146379de80be968
nm1008 <mallari08nikko@yahoo.com>
2023-08-09 00:02:14 +0800

    fixed bug error message / implement toggle read/unread button

--- script.js
+++ script.js
@@ -6,45 +6,15 @@ const formContainer = document.querySelector(".form-container");
 
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-}
+    this.read = read;
+}    
 
-function addBookToLibrary() {
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const newBook = new Book(title, author, pages)
-    myLibrary.push(newBook)
-    formContainer.style.display = "none"
-    renderLibrary()
-}
-
-function renderLibrary(){
-    let container = document.querySelector(".container");
-    container.innerHTML = ""
-        for(let i = 0; i < myLibrary.length; i++){
-
-
-            let book = myLibrary[i]
-            let createCard = document.createElement("div");
-                if(book.title === "" || book.author === "" || book.pages === null){
-                    alert("Error")
-                }else{
-                    createCard.innerHTML = 
-                    `
-                    <div class="card">
-                        <h2 class="title"> ${book.title}</h2>
-                        <h4 class="author">By:${book.author}</h4>
-                        <p>Pages: ${book.pages}</p>
-                        <button class="remove" onclick="removeBook()">Remove</button>
-                    </div>
-                    `
-                }
-        container.appendChild(createCard)
-    }
+Book.prototype.toggleRead = function (){
+    this.read = !this.read;
 }
 
 function removeBook(index){
@@ -52,6 +22,50 @@ function removeBook(index){
     renderLibrary();
 }
 
+function readBook(index){
+    myLibrary[index].toggleRead();
+    renderLibrary()
+}
+
+
+function addBookToLibrary() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const readCheckbox = document.querySelector("#read");
+    const read = readCheckbox.checked;
+    const newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
+    formContainer.style.display = "none"
+    renderLibrary();
+}
+
+function renderLibrary(){
+    let container = document.querySelector(".container");
+    container.innerHTML = ""
+        for(let i = 0; i < myLibrary.length; i++){
+            let book = myLibrary[i]
+            let createCard = document.createElement("div");
+            createCard.innerHTML = 
+            `
+            <div class="card">
+                <h2 class="title">${book.title}</h2>
+                <h4 class="author">By: ${book.author}</h4>
+                <p>Pages: ${book.pages}</p>
+               
+                <div class="buttons">
+                    <button class="read" onclick="readBook(${i})"
+                    style="background-color: ${book.read ? 'green' : 'red'}; border-color: ${book.read ? 'green' : 'red'}">
+                    ${book.read ? "Read" : "Not yet"}
+                    </button>
+                    <button class="remove" onclick="removeBook(${i})">Remove</button>
+                </div>
+                
+            </div>
+            `    
+         container.appendChild(createCard)
+    }
+}
 
 newBookBtn.addEventListener("click", () => {
     formContainer.style.display = "flex"
@@ -65,7 +79,6 @@ submitForm.addEventListener("click", (e) => {
 
     inputs.forEach(input => {
         input.value = "";
+        input.checked = false;
     })
 })
-
-
